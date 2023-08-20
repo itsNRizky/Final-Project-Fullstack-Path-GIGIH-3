614a84883e1542ec7c88da9c961e196b95a28a96
Novian Rizky <79080026+itsNRizky@users.noreply.github.com>
2023-08-20 10:42:23 +0700

    Update mongodb.js

--- backend/configs/mongodb.js
+++ backend/configs/mongodb.js
@@ -1,18 +1,20 @@
-const dotenv = require('dotenv')
-const mongoose = require('mongoose')
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-dotenv.config()
+dotenv.config();
 
-const mongodbURI = process.env.MONGODB_URI
-console.log(mongodbURI)
+const mongodbURI = process.env.MONGODB_URI;
+console.log(mongodbURI);
 
-async function connectToMongoDB(){
+async function connectToMongoDB() {
   try {
-    await mongoose.connect(mongodbURI)
-    console.log('Connected to database!')
-  } catch (err){
-    console.error(`DB Connection error: ${err.message}`)
+    await mongoose.connect(
+      "mongodb+srv://nrizky:novian123@study.9ysim6p.mongodb.net/finalterm/?retryWrites=true&w=majority"
+    );
+    console.log("Connected to database!");
+  } catch (err) {
+    console.error(`DB Connection error: ${err.message}`);
   }
 }
 
-module.exports = connectToMongoDB
+module.exports = connectToMongoDB;
